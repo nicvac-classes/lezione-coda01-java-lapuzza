25833f2cc59ad8418b2f0fd1066dfdfe82b45ced
FNC-pinoFraddosio <benedetto.fraddosio@marconibari.edu.it>
2026-05-04 20:34:08 +0000

    prima consegna

--- Coda.java
+++ Coda.java
@@ -1,1 +1,15 @@
 //Implementare qui la classe Coda
+public class Coda<T> {
+    private Nodo<T> head;
+    private Nodo<T> tail;
+
+    public Coda(){
+        this.head = null;
+        this.tail = null;
+    }
+    
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+}
--- Nodo.java
+++ Nodo.java
@@ -1,1 +1,11 @@
 //Implementare qui la classe Nodo
+public class Nodo<T> {
+    T dato;
+    Nodo<T> next;
+
+    public Nodo(T datoT){
+        this.dato = dato;
+        this.next = null;
+    }
+    
+} 
